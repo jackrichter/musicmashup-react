8ede68f6143149a3c9581585ea230d02ddac1aa3
jackrichter <jackrichter1@gmail.com>
2024-03-08 08:42:09 +0100

    cleanup + port 8080 for preview.

--- musicmashupapp/src/components/ViewMusicData.jsx
+++ musicmashupapp/src/components/ViewMusicData.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 import FooterComponent from "./FooterComponent";
-import axios from "axios";
 import Background from "./Background";
 import JSONPretty from "json-stringify-pretty-compact";
 import LogoForSearchResult from "./LogoForSearchResult";
@@ -13,46 +12,26 @@ const ViewMusicData = () => {
   const musicDataRef = useRef(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:8181/music/musicbrainz/" + mbid);
-        // Assuming the response data is in JSON format
-        setResponseData(JSON.stringify(response.data));
-        musicDataRef.current = JSON.stringify(response.data);
-        // console.log("Ref: ", musicDataRef.current);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    function fetchData2() {
+    function fetchData() {
       fetch("http://localhost:8181/music/musicbrainz/" + mbid)
         .then(res => res.json())
         .then(data => {
           setResponseData(data);
-          console.log(data);
           musicDataRef.current = data;
-          console.log("musicDataRef");
-          console.log(musicDataRef.current);
         });
     }
 
-    // fetchData();
-    fetchData2();
+    fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  var data = musicDataRef.current;
-  // console.log(data);
-  // console.log("ResponseData: ", responseData);
+  var displayData = musicDataRef.current;
   const jsonDisplay = JSONPretty(musicDataRef.current, { indent: 2 });
-  // const jsonDisplay = pretty.replace(/{|}/g, "");
-  // console.log("Ref Pretty: ", jsonDisplay);
 
   return (
     <div className="container py-md-5">
       <Background />
-      {data ? (
+      {displayData ? (
         <div className="row d-flex justify-content-center">
           <textarea readOnly="true" id="textarea" className="form-form-control-color info-div" cols={200} rows={30} value={jsonDisplay}></textarea>
         </div>
